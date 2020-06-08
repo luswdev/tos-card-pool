19ae7742efab662e2d98d1a500fa300fbbb36e06
Callum Lu <610721230@gms.ndhu.edu.tw>
2020-06-08 13:08:42 +0800

    update to gen-16th

--- asserts/js/render.js
+++ asserts/js/render.js
@@ -1,6 +1,15 @@
-$(document).ready(function(){
+/**
+ * render.js
+ */
+
+'use strict';
+
+$(document).ready( function () {
+
+    /* initial cookies at enter site first */
     cookies_init(); 
 
+    /* get data from JSON */
     $.getJSON('/data/config.json', function(json) {
         var card_id = 0;
 
@@ -15,7 +24,7 @@ $(document).ready(function(){
                     var inv = get_val('inverse_pick') != null ? get_val('inverse_pick') : Cookies.get('inverse_pick');
                     return inv == '1' ? 'inverse-on' : '';
                 },
-                click_inv: function() {
+                click_inv: function () {
                     this.inverse = ~this.inverse;
                     var bufi = parseInt(Cookies.get('inverse_pick'), 10);
                         
@@ -128,7 +137,9 @@ $(document).ready(function(){
     });
 });
 
-function cookies_init() {
+/* initial cookies */
+function cookies_init() 
+{
     if (Cookies.get('pick_history') == undefined) {
         Cookies.set('pick_history', '0', { expires: 365, path: '/' }); 
     } else {
@@ -142,8 +153,10 @@ function cookies_init() {
     }
 }
 
-function cookies_update() {
-    var last_card_id = $('.card-box').last().attr('id').split('-')[1],
+/* update cookies */
+function cookies_update() 
+{
+    let last_card_id = $('.card-box').last().attr('id').split('-')[1],
         total_col = parseInt(last_card_id / 4),
         current_cookies = Cookies.get('pick_history'),
         inverse = Cookies.get('inverse_pick');
@@ -157,21 +170,27 @@ function cookies_update() {
     Cookies.set('inverse_pick', inverse, { expires: 365, path: '/' });
 }
 
-function materialize_init() {
+/* materialize components initial */
+function materialize_init() 
+{
+    /* initial modal */
     $('.modal').modal({
         'endingTop': '35%'
     });
 
+    /* initial fixed button */
     $('.fixed-action-btn').floatingActionButton({
         direction: 'top',
         hoverEnabled: false
     });
 
+    /* initial cards name tooltips */
     $('.tooltipped').tooltip({
         enterDelay: 1000
     });
 }
 
+/* helper function: replace string */
 function replace_string_at(str, ind, val)
 {
     if (ind < 0 || ind > str.length) {
@@ -181,7 +200,9 @@ function replace_string_at(str, ind, val)
     return str.substr(0,ind) + val + str.substr(ind+1);
 }
 
-function get_val(par) {
+/* get picker value at GET url */
+function get_val(par) 
+{
     let urlParams = new URLSearchParams(window.location.search);
     var val = urlParams.get(par);
 
